2014b4d83fe87faeb9541a64e73fb90e4c6c45fd
areinsvo <areinsvo@nd.edu>
2019-02-19 21:53:26 +0100

    Add missing LinkDef file

---
+++ Analysis/AllieCode/SusyNtuplizer_LinkDef.h
@@ -0,0 +1,34 @@
+// -*- C++ -*-
+//
+/*
+
+ Description: Dictionary generator
+
+ Implementation:
+
+*/
+#include "../src/ggEventTree.h"
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link off all typedefs;
+
+#pragma link C++ class std::pair<TString, UChar_t>+;
+#pragma link C++ class std::map<TString, UChar_t>+;
+#pragma link C++ class std::pair<TString, Float_t>+;
+#pragma link C++ class std::map<TString, Float_t>+;
+#pragma link C++ class std::pair<TString, TVector3>+;
+#pragma link C++ class std::map<TString, TVector3>+;
+#pragma link C++ class std::pair<TString, TLorentzVector>+;
+#pragma link C++ class std::map<TString, TLorentzVector>+;
+
+#pragma link C++ class ggEventTree+;
+
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+#pragma link C++ class std::pair<UInt_t, Bool_t>+;
+
+#endif
